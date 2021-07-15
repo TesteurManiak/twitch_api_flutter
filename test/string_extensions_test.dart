416478069b9ff7cfe41e68b35f3dcb4ab37e1b76
Guillaume Roux <rouxguillaume8@gmail.com>
2021-07-15 13:13:29 +0200

    finished test for string_extensions

--- test/string_extensions_test.dart
+++ test/string_extensions_test.dart
@@ -2,6 +2,7 @@ import 'package:test/test.dart';
 import 'package:twitch_api/src/exceptions/twitch_api_exception.dart';
 import 'package:twitch_api/src/extensions/string_extensions.dart';
 import 'package:twitch_api/src/models/twitch_cheermote_type.dart';
+import 'package:twitch_api/src/models/twitch_custom_reward_redemption.dart';
 import 'package:twitch_api/src/models/twitch_extension_transaction.dart';
 import 'package:twitch_api/src/models/twitch_stream_info.dart';
 
@@ -46,6 +47,25 @@ void main() {
       );
       expect(
         () => 'BITS_IN_CHANNEL'.toTransactionProductType(),
+        throwsA(isA<TwitchApiException>()),
+      );
+    });
+
+    test('toRewardRedemptionStatus()', () {
+      expect(
+        'UNFULFILLED'.toRewardRedemptionStatus(),
+        TwitchRewardRedemptionStatus.unfulfilled,
+      );
+      expect(
+        'FULFILLED'.toRewardRedemptionStatus(),
+        TwitchRewardRedemptionStatus.fulfilled,
+      );
+      expect(
+        'CANCELED'.toRewardRedemptionStatus(),
+        TwitchRewardRedemptionStatus.canceled,
+      );
+      expect(
+        () => 'error'.toRewardRedemptionStatus(),
         throwsA(isA<TwitchApiException>()),
       );
     });
