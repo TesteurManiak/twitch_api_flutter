6dc90dc3c9316462492c9c38902c3d5f9137d0d2
Guillaume Roux <rouxguillaume8@gmail.com>
2021-07-15 13:10:35 +0200

    add tests for string_extensions
    toStreamType()
    toCheermoteType()
    toTransactionProductType()

---
+++ test/string_extensions_test.dart
@@ -0,0 +1,53 @@
+import 'package:test/test.dart';
+import 'package:twitch_api/src/exceptions/twitch_api_exception.dart';
+import 'package:twitch_api/src/extensions/string_extensions.dart';
+import 'package:twitch_api/src/models/twitch_cheermote_type.dart';
+import 'package:twitch_api/src/models/twitch_extension_transaction.dart';
+import 'package:twitch_api/src/models/twitch_stream_info.dart';
+
+void main() {
+  group('String Extensions', () {
+    test('toStreamType()', () {
+      expect('live'.toStreamType(), TwitchStreamType.live);
+      expect('error'.toStreamType(), TwitchStreamType.error);
+    });
+
+    test('toCheermoteType()', () {
+      expect(
+        'global_first_party'.toCheermoteType(),
+        TwitchCheermoteType.globalFirstParty,
+      );
+      expect(
+        'global_third_party'.toCheermoteType(),
+        TwitchCheermoteType.globalThirdParty,
+      );
+      expect(
+        'channel_custom'.toCheermoteType(),
+        TwitchCheermoteType.channelCustom,
+      );
+      expect(
+        'display_only'.toCheermoteType(),
+        TwitchCheermoteType.displayOnly,
+      );
+      expect(
+        'sponsored'.toCheermoteType(),
+        TwitchCheermoteType.sponsored,
+      );
+      expect(
+        () => 'error'.toCheermoteType(),
+        throwsA(isA<TwitchApiException>()),
+      );
+    });
+
+    test('toTransactionProductType()', () {
+      expect(
+        'BITS_IN_EXTENSION'.toTransactionProductType(),
+        TwitchExtensionTransactionProductType.bitsInExtension,
+      );
+      expect(
+        () => 'BITS_IN_CHANNEL'.toTransactionProductType(),
+        throwsA(isA<TwitchApiException>()),
+      );
+    });
+  });
+}
