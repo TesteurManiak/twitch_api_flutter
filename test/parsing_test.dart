7ffdd096da7b85a029cbc294684dac3a8e9b55e7
Lewis Holliday <83846295+LewisHolliday@users.noreply.github.com>
2022-07-28 07:40:07 +0100

    added json response test case where tagIds is null

--- test/parsing_test.dart
+++ test/parsing_test.dart
@@ -112,6 +112,36 @@ void main() {
         expect(twitchStreamInfo.isMature, false);
       });
     });
+    test('Empty Tags', () {
+        final json = jsonDecode(readFileStringSync('get_streams_3.json'));
+        final obj = TwitchResponse<TwitchStreamInfo>.streamsInfo(
+          json as Map<String, dynamic>,
+        );
+
+        expect(obj.data!.length, 1);
+
+        final twitchStreamInfo = obj.data!.first;
+        expect(twitchStreamInfo.id, '41375541868');
+        expect(twitchStreamInfo.userId, '459331509');
+        expect(twitchStreamInfo.userLogin, 'auronplay');
+        expect(twitchStreamInfo.userName, 'auronplay');
+        expect(twitchStreamInfo.gameId, '494131');
+        expect(twitchStreamInfo.gameName, 'Little Nightmares');
+        expect(twitchStreamInfo.type, TwitchStreamType.live);
+        expect(
+          twitchStreamInfo.title,
+          'hablamos y le damos a Little Nightmares 1',
+        );
+        expect(twitchStreamInfo.viewerCount, 78365);
+        expect(twitchStreamInfo.language, 'es');
+        expect(
+          twitchStreamInfo.thumbnailUrl,
+          'https://static-cdn.jtvnw.net/previews-ttv/live_user_auronplay-{width}x{height}.jpg',
+        );
+        expect(twitchStreamInfo.tagIds.length, 0);
+        expect(twitchStreamInfo.isMature, false);
+      });
+    
 
     group('Channel Editors', () {
       test('1', () {
